121b9d0f3ff3c29ad9627108965fe2f05f2e4b08
63070188 <68292431+63070188@users.noreply.github.com>
2022-10-18 11:40:13 +0700

    Add files via upload

---
+++ productservice/command/ProductAggregate.java
@@ -0,0 +1,48 @@
+package com.sop.chater7.productservice.command;
+
+import com.sop.chater7.productservice.core.event.PrtoductCreatedEvent;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
+
+import java.math.BigDecimal;
+
+@Aggregate
+public class ProductAggregate {
+
+    @AggregateIdentifier
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+
+    public ProductAggregate() {
+
+    }
+
+    @CommandHandler
+    public ProductAggregate(CreateProductCommand createProductCommand) {
+        if(createProductCommand.getPrice().compareTo(BigDecimal.ZERO) <= 0){
+            throw new IllegalArgumentException("Price cannot be less than or equal to zero");
+        }
+
+        if(createProductCommand.getTitle() == null || createProductCommand.getTitle().isBlank()){
+            throw new IllegalArgumentException("Title cannot be empty");
+        }
+
+        PrtoductCreatedEvent prtoductCreatedEvent = new PrtoductCreatedEvent();
+        BeanUtils.copyProperties(createProductCommand, prtoductCreatedEvent);
+        AggregateLifecycle.apply(prtoductCreatedEvent);
+    }
+
+    @EventSourcingHandler
+    public void on(PrtoductCreatedEvent prtoductCreatedEvent){
+        this.productId = prtoductCreatedEvent.getProductId();
+        this.title = prtoductCreatedEvent.getTitle();
+        this.price = prtoductCreatedEvent.getPrice();
+        this.quantity = prtoductCreatedEvent.getQuantity();
+    }
+}
